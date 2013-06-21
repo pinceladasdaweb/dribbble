515c3a46e47fa05074e831c9466bfcc11238a00f
Pedro Rogerio <pedro.lemes@locaweb.com.br>
2013-06-21 18:03:11 -0300

    Indent code

--- src/dribbble.js
+++ src/dribbble.js
@@ -1,32 +1,32 @@
 var Dribbble = {
-	init: function(config) {
-		this.url = 'http://api.dribbble.com/players/'+config.username+'/shots?per_page='+config.count+'&callback=?'
-		this.template = config.template;
+    init: function(config) {
+        this.url = 'http://api.dribbble.com/players/'+config.username+'/shots?per_page='+config.count+'&callback=?';
+        this.template = config.template;
         this.container = config.container;
-        this.fetch();	
-	},
-	attachTemplate: function() {
-		var template = Handlebars.compile(this.template);
+        this.fetch();   
+    },
+    attachTemplate: function() {
+        var template = Handlebars.compile(this.template);
 
         this.container.empty().append(template(this.shots));
-	},
-	fetch: function() {
-		var self = this;
-		
-		$.getJSON(this.url, function(data) {
-			var feed = data.shots;
-			
-			self.shots = $.map(feed, function(shot) {
-				return {
+    },
+    fetch: function() {
+        var self = this;
+        
+        $.getJSON(this.url, function(data) {
+            var feed = data.shots;
+            
+            self.shots = $.map(feed, function(shot) {
+                return {
                     image: shot.image_teaser_url,
-					title: shot.title,
-					link: shot.url
+                    title: shot.title,
+                    link: shot.url
                 };
-			});
-			
-			self.attachTemplate();
-		});	
-	}
+            });
+            
+            self.attachTemplate();
+        }); 
+    }
 }
 
 Dribbble.init({
